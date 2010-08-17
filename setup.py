365778fd13d60892c54d7096e5eab84d0ee0fc7e
Robert Bauck Hamar <r.b.hamar@usit.uio.no>
2010-08-17 23:51:20 +0200

    Adding distutils installer

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2010 University of Oslo, Norway
+#
+# This file is part of PyBofh.
+#
+# PyBofh is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBofh is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyBofh; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+from distutils.core import setup
+
+setup(name='pybofh',
+      description='Cerebrum Bofh client',
+      version='0.2',
+      license='GPL',
+      scripts=['scripts/bofh', 'scripts/passwd'],
+      packages=['bofh'])
+
