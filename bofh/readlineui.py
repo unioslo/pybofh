3c9e20729b5f61e18e76f48c6b78dddd1a0357a3
Jonas Braathen <jonas.braathen@usit.uio.no>
2018-01-22 13:42:53 +0100

    Fix exit codes on KeyboardInterrupt/EOFError

--- bofh/readlineui.py
+++ bofh/readlineui.py
@@ -217,7 +217,8 @@ def repl(bofh, charset=None, prompt=None):
             print "So long, and thanks for all the fish!"
             return
         except KeyboardInterrupt:
-            raise SystemExit("")
+            print ""
+            raise SystemExit
         if script_file is not None:
             script_file.write("%s %s\n" % (prompt, line.encode(charset)))
         try:
