cde61215eb36586870cf17dba5651c47f7e988c9
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2018-10-08 17:19:55 +0200

    Rewrite setup.py
    - Removes mercurial versioning hack
    - Replaces distutils with setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Copyright 2010 University of Oslo, Norway
+#
+# Copyright 2010-2018 University of Oslo, Norway
 #
 # This file is part of PyBofh.
 #
@@ -18,112 +18,105 @@
 # You should have received a copy of the GNU General Public License
 # along with PyBofh; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+import io
 
-from distutils.core import setup
-from distutils.cmd import Command
-
-class archival(object):
-
-    def __init__(self):
-        self.latesttag = []
-        self.tag = []
-        self.latesttagdistance = -1
-        self.repo = self.node = self.branch = None
-        input = None
-        try:
-            input = open(".hg_archival.txt", "r")
-            self.parse_archival(input)
-        except IOError:
-            pass
-        finally:
-            if input is not None:
-                input.close()
-
-    def parse_archival(self, input):
-        for k, v in [x.split(":", 1) for x in  input.readlines()]:
-            if k in ('repo', 'node', 'branch'):
-                setattr(self, k, v.strip())
-            elif k == 'tag':
-                self.tag.append(v.strip())
-            elif k == 'latesttag':
-                self.latesttag.append(v.strip())
-            elif k == 'latesttagdistance':
-                self.latesttagdistance = int(v.strip())
+import setuptools
+import setuptools.command.test
 
 
-def archive_version():
-    ar = archival()
-    for tag in ar.tag:
-        if tag.startswith('version-'):
-            return tag.split('-', 1)[1]
-    if ar.node:
-        return ar.node[:12]
+def get_requirements(filename):
+    """ Read requirements from file. """
+    with io.open(filename, mode='rt', encoding='utf-8') as f:
+        for line in f:
+            # TODO: Will not work with #egg-info
+            requirement = line.partition('#')[0].strip()
+            if not requirement:
+                continue
+            yield requirement
 
 
-def hg_version():
-    import os
-    import mercurial.ui
-    import mercurial.localrepo
-    ui = mercurial.ui.ui()
-    repo = mercurial.localrepo.localrepository(ui, os.getcwd())
-    ctx = repo['.']
-    for tag in ctx.tags():
-        if tag.startswith('version-'):
-            return tag.split('-', 1)[1]
-    return ctx.hex()[:12]
+def get_textfile(filename):
+    """ Get contents from a text file. """
+    with io.open(filename, mode='rt', encoding='utf-8') as f:
+        return f.read().lstrip()
 
 
-def bofh_version():
-    from bofh.version import version
-    return version
+def get_packages():
+    """ List of (sub)packages to install. """
+    return setuptools.find_packages('.', include=('bofh', 'bofh.*'))
+
+
+class PyTest(setuptools.command.test.test):
+    """ Run tests using pytest.
+
+    From `http://doc.pytest.org/en/latest/goodpractices.html`.
+
+    """
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = []
+
+    def run_tests(self):
+        import shlex
+        import pytest
+        args = self.pytest_args
+        if args:
+            args = shlex.split(args)
+        errno = pytest.main(args)
+        raise SystemExit(errno)
 
 
 def get_version():
-    av = archive_version()
-    if av:
-        return av
     try:
-        hv = hg_version()
-        return hv
-    except:
-        return bofh_version()
+        from bofh.version import version
+        return version
+    except Exception:
+        return u'unknown'
 
 
-def replace_copy_file(version):
-    old = Command.copy_file
+def main():
+    setup_requirements = []  # ['setuptools_scm']
+    test_requirements = list(get_requirements('requirements-test.txt'))
+    install_requirements = list(get_requirements('requirements.txt'))
+    version = get_version()
 
-    def write_content(fname):
-        f = open(fname, "w")
-        f.write('version = %r' % version)
-        f.close()
+    setuptools.setup(
+        name='pybofh',
+        description='Cerebrum Bofh client',
+        long_description=get_textfile('README.md'),
+        long_description_content_type='text/markdown',
 
-    def copy_file (self, infile, outfile,
-        preserve_mode=1, preserve_times=1, link=None, level=1):
-        """Copy a file respecting verbose, dry-run and force flags.  (The
-        former two default to whatever is in the Distribution object, and
-        the latter defaults to false for commands that don't define it.)"""
+        url='https://bitbucket.usit.uio.no/projects/CRB/repos/pybofh',
+        author='USIT, University of Oslo',
+        author_email='bnt-int@usit.uio.no',
 
-        if infile.endswith("version.py"):
-            if self.dry_run:
-                return outfile, 1
-            try:
-                self.make_file([infile], outfile, write_content, (outfile,), level=level)
-            except IOError:
-                return outfile, 0
-            return outfile, 1
+        version=version,
+        packages=get_packages(),
+        scripts=['scripts/bofh', 'scripts/passwd'],
+        setup_requires=setup_requirements,
+        install_requires=install_requirements,
+        tests_require=test_requirements,
+        license='GPLv3',
+        package_data={'bofh.ext': ['LICENCE.txt', 'README.md'], },
+        data_files=[('etc/pybofh', ['data/ca.pem', ]), ],
 
-        return old(self, infile, outfile, preserve_mode, preserve_times, link, level)
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: GNU General Public License v3',
+            'Programming Language :: Python :: 2 :: Only',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: System :: Systems Administration',
+        ],
+        keywords='cerebrum xmlrpc client',
+    )
 
-    Command.copy_file = copy_file
 
-version = get_version()
-replace_copy_file(unicode(version))
-
-setup(name='pybofh',
-      description='Cerebrum Bofh client',
-      version=version,
-      license='GPL',
-      scripts=['scripts/bofh', 'scripts/passwd'],
-      packages=['bofh', 'bofh.ext'],
-      package_data={'bofh.ext': ['LICENCE.txt', 'README.md'], },
-      data_files=[('etc/pybofh', ['data/ca.pem', ]), ])
+if __name__ == '__main__':
+    main()
