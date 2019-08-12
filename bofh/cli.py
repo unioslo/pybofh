e9aa0dd90b43e4237645fe6637797f04556519c2
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-08-12 13:47:09 +0200

    Add command cli argument and fix a unicode issue
    Adds a repeatable '--cmd' command line argument. If one or more
    arguments are given, the pybofh script will run each command and exit.
    
    Also fixes a unicode issue with some cli arguments on python2

--- bofh/cli.py
+++ bofh/cli.py
@@ -23,12 +23,16 @@ from __future__ import print_function, unicode_literals
 
 import argparse
 import getpass
+import locale
 import logging
+import sys
 
+import six
 from six.moves.urllib.parse import urlparse
 
 import bofh
 import bofh.config
+import bofh.parser
 import bofh.proto
 import bofh.readlineui
 import bofh.version
@@ -63,11 +67,6 @@ def complete_url(url):
     return url
 
 
-def prompt_password(user):
-    """ password prompt for `user` """
-    return getpass.getpass('Password for {}:'.format(user))
-
-
 def setup_logging(verbosity):
     """ configure logging if verbosity is not None """
     if verbosity is None:
@@ -78,9 +77,50 @@ def setup_logging(verbosity):
         bofh.config.configure_logging(level)
 
 
+def get_default_encoding():
+    """ guess encoding for arguments. """
+
+    def _iter_encodings():
+        yield locale.getdefaultlocale()[1]
+        yield sys.getfilesystemencoding()
+        yield sys.getdefaultencoding()
+
+    for encoding in _iter_encodings():
+        if encoding:
+            return encoding
+
+
+def bofh_eval(conn, line):
+    parse = bofh.parser.parse(conn, line)
+    logger.debug("Got obj=%s, command=%r",
+                 repr(parse), repr(getattr(parse, 'command', None)))
+    result = parse.eval(prompter=None)
+    logger.debug("Got result=%s", repr(result))
+    if isinstance(result, list):
+        result = '\n\n'.join(result)
+    return result
+
+
+class UnicodeType(object):
+    """ Argparse transform for non-unicode input. """
+
+    def __init__(self, encoding=None):
+        self.encoding = encoding
+
+    def __call__(self, value):
+        if isinstance(value, bytes):
+            value = value.decode(self.encoding)
+        return six.text_type(value)
+
+    def __repr__(self):
+        return 'UnicodeType(encoding={0})'.format(repr(self.encoding))
+
+
 def main(inargs=None):
     parser = argparse.ArgumentParser(
-        description="The Cerebrum Bofh client")
+        description="The Cerebrum Bofh client",
+    )
+    default_encoding = get_default_encoding()
 
     parser.add_argument(
         '--version',
@@ -126,6 +166,7 @@ def main(inargs=None):
     output_args.add_argument(
         '-p', '--prompt',
         default=bofh.readlineui.DEFAULT_PROMPT,
+        type=UnicodeType(default_encoding),
         help="use a custom prompt (default: %(default)r)",
         metavar='PROMPT',
     )
@@ -134,11 +175,23 @@ def main(inargs=None):
         dest='verbosity',
         action='count',
         default=None,
-        help="show debug messages on stderr")
+        help="show debug messages on stderr",
+    )
+
+    parser.add_argument(
+        '--cmd',
+        action='append',
+        dest='commands',
+        type=UnicodeType(default_encoding),
+        help="run command %(metavar)s and exit",
+        metavar='CMD',
+    )
 
     args = parser.parse_args(inargs)
     setup_logging(args.verbosity)
     logger.debug('args: %r', args)
+
+    prompt_pass = bofh.readlineui.IOUtil(encoding=default_encoding).get_secret
 
     print("Connecting to {}\n".format(complete_url(args.url)))
     try:
@@ -148,7 +201,8 @@ def main(inargs=None):
                             timeout=args.timeout)
         if conn.motd:
             print(conn.motd)
-        conn.login(args.user, prompt_password(args.user))
+        conn.login(args.user,
+                   prompt_pass('Password for {}:'.format(args.user)))
     except (KeyboardInterrupt, EOFError):
         print("")
         raise SystemExit()
@@ -157,7 +211,14 @@ def main(inargs=None):
 
     # Start the interactive REPL
     try:
-        bofh.readlineui.repl(conn, prompt=args.prompt)
+        if args.commands:
+            for command in args.commands:
+                print(args.prompt + command)
+                # ugh
+                sys.stdout.flush()
+                print(bofh_eval(conn, command))
+        else:
+            bofh.readlineui.repl(conn, prompt=args.prompt)
     except Exception as e:
         logger.error("Unhandled error", exc_info=True)
         raise SystemExit('Error: {}'.format(e))
